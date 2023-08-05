a2cfa82f08009e47195bbcf5c5aa6dd9c9586932
Amit Gawande <amit.w.gawande@gmail.com>
2023-08-05 17:16:18 +0530

    Rename the referer header

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ cors_proxy.createServer({
     'via',
     'connect-time',
     'total-route-time',
-    'Referer',
+    'referer',
     // Other Heroku added debug headers
     // 'x-forwarded-for',
     // 'x-forwarded-proto',
